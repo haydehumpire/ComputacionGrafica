0519dd9b24fac2044d6ced6309bb23a18c2cab09
Hayde Luzmila Hc <hayde.humpire@gmail.com>
2019-10-11 00:21:23 -0500

    Add files via upload

---
+++ Codigos/RotacionEscalarTraslacion.cpp
@@ -0,0 +1,100 @@
+#include <GL/glut.h>
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+void drawLineIncremental(float x1,float y1,float z1,float x2,float y2,float z2) {
+   float x,y,z;
+   float a;
+   glBegin(GL_POINTS);
+       a = (y2-y1)/(x2-x1);
+       for (z =z1; z<=z2;)
+       {
+           for(x=x1; x<=x2;){
+              y = (y1+ a*(x-x1));
+              glVertex3f(x,y,z);
+              x+=0.01;
+           }
+           z+=0.01;
+        }
+
+   glEnd();
+}
+
+
+void display(){
+    glClear( GL_COLOR_BUFFER_BIT );
+    glColor3ub( 255, 255, 0 );
+    drawLineIncremental(0,0,0,0.8,0.5,0.5);
+    glutSwapBuffers();
+}
+
+
+void Move(unsigned char key, int x, int y)
+{
+   switch(key) {
+
+
+    /// TRASLACION
+    case 'w' :
+        cout<<"Arriba"<<endl;
+        glTranslatef(0.0, 0.1 ,0.0);
+        break;
+    case 'a' :
+        cout<<"Izquierda"<<endl;
+        glTranslatef(-0.1, 0.0 ,0.0);
+        break;
+    case 's' :
+        cout<<"Abajo"<<endl;
+        glTranslatef(0.0, -0.1, 0);
+        break;
+    case 'd' :
+        cout<<"Derecha"<<endl;
+        glTranslatef(0.1, 0 ,0.0);
+        break;
+
+    /// ROTACION
+    case 'i' :
+        cout<<"Arriba"<<endl;
+        glRotatef(5.0, 1.0, 0.0, 0.0);
+        break;
+    case 'j' :
+        cout<<"Izquierda"<<endl;
+        glRotatef(-5.0, 0.0, 1.0, 0.0);
+        break;
+    case 'k' :
+        cout<<"Abajo"<<endl;
+        glRotatef(-5.0, 1.0, 0.0, 0.0);
+        break;
+    case 'l' :
+        cout<<"Derecha"<<endl;
+        glRotatef(5.0, 0.0, 1.0, 0.0);
+        break;
+
+    /// SCALAR
+    case '+' :
+        glScalef(2,2,2);
+        break;
+
+    case '-' :
+        glScalef(0.5,0.5,0.5);
+        break;
+    }
+    glutPostRedisplay();
+}
+
+int main( int argc, char **argv )
+{
+    glutInit( &argc, argv );
+    glutInitDisplayMode( GLUT_RGBA | GLUT_DOUBLE );
+    glutInitWindowSize( 350, 350 );
+    glutInitWindowPosition(0, 0);
+    glutCreateWindow( "Linea 3D - Rotacion, Escalar y Traslacion" );
+
+    glutKeyboardFunc(Move);
+
+    glutDisplayFunc( display );
+    glutMainLoop();
+    return 0;
+}
