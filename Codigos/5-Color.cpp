c1b4f59d24e2ebf3074d052a21893880ba66dee0
Hayde Luzmila Humpire Cutipa <hayde.humpire@gmail.com>
2019-10-12 23:01:02 -0500

    Pintado del poligono

--- Codigos/5-Color.cpp
+++ Codigos/5-Color.cpp
@@ -3,31 +3,31 @@
 
 GLfloat x1,y1,x2,y2,x3,y3,x4,y4;
 
-void edgedetect(GLfloat x1,GLfloat y1,GLfloat x2,GLfloat y2,int *le,int *re){
+void Deteccion_Borde(GLfloat x1,GLfloat y1,GLfloat x2,GLfloat y2,int *le,int *re){
     float mx,x,temp;
     int i;
     if((y2-y1)<0)
     {
-    	temp=x1;x1=x2;x2=temp;
-    	temp=y1;y1=y2;y2=temp;
+        temp=x1;x1=x2;x2=temp;
+        temp=y1;y1=y2;y2=temp;
     }
     if((y2-y1)!=0)
-    	mx=(x2-x1)/(y2-y1);
+        mx=(x2-x1)/(y2-y1);
     else
-    	mx=x2-x1;
-    	x=x1;
+        mx=x2-x1;
+        x=x1;
     for(i=y1;i<y2;i++)
     {
-		if(x<le[i])
+        if(x<le[i])
             le[i]=x;
 
-		if(x>re[i])
+        if(x>re[i])
             re[i]=x;
-		x+=mx;
+        x+=mx;
     }
 }
 
-void drawpixel(int x,int y)
+void Dibujar_Pixeles(int x,int y)
 {
     glColor3f(1.0,0,1.0);
     glBegin(GL_POINTS);
@@ -35,7 +35,7 @@ void drawpixel(int x,int y)
     glEnd();
 }
 
-void scanfill(float x1,float y1,float x2,float y2,float x3,float y3,float x4,float y4){
+void Scanfill(float x1,float y1,float x2,float y2,float x3,float y3,float x4,float y4){
     int le[500],re[500];
     int i,y;
     for(i=0;i<500;i++)
@@ -43,15 +43,15 @@ void scanfill(float x1,float y1,float x2,float y2,float x3,float y3,float x4,flo
         le[i]=500;
         re[i]=0;
     }
-    edgedetect(x1,y1,x2,y2,le,re);
-    edgedetect(x2,y2,x3,y3,le,re);
-    edgedetect(x3,y3,x4,y4,le,re);
-    edgedetect(x4,y4,x1,y1,le,re);
+    Deteccion_Borde(x1,y1,x2,y2,le,re);
+    Deteccion_Borde(x2,y2,x3,y3,le,re);
+    Deteccion_Borde(x3,y3,x4,y4,le,re);
+    Deteccion_Borde(x4,y4,x1,y1,le,re);
     for(y=0;y<500;y++)
     {
         if(le[y]<=re[y])
         for(i=le[y]+1;i<re[y];i++)
-        drawpixel(i,y);
+        Dibujar_Pixeles(i,y);
     }
 }
 
@@ -64,7 +64,7 @@ void display(){
     glVertex2f(x3,y3);
     glVertex2f(x4,y4);
     glEnd();
-    scanfill(x1,y1,x2,y2,x3,y3,x4,y4);
+    Scanfill(x1,y1,x2,y2,x3,y3,x4,y4);
     glFlush();
 }
 
