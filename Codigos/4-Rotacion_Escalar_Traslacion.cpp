bdaba7cf984915ba593dfa3433207f4f2791e62b
Hayde Luzmila Humpire Cutipa <hayde.humpire@gmail.com>
2019-10-12 23:56:32 -0500

    Grafica de laTraslacion, Rotacion y Escalar

--- Codigos/4-Rotacion_Escalar_Traslacion.cpp
+++ Codigos/4-Rotacion_Escalar_Traslacion.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-void drawLineIncremental(float x1,float y1,float z1,float x2,float y2,float z2) {
+void Dibujar_Linea_Incremental(float x1,float y1,float z1,float x2,float y2,float z2) {
    float x,y,z;
    float a;
    glBegin(GL_POINTS);
@@ -22,19 +22,24 @@ void drawLineIncremental(float x1,float y1,float z1,float x2,float y2,float z2) 
    glEnd();
 }
 
-
-void display(){
+void Mostrar(){
     glClear( GL_COLOR_BUFFER_BIT );
     glColor3ub( 255, 255, 0 );
-    drawLineIncremental(0,0,0,0.8,0.5,0.5);
+    Dibujar_Linea_Incremental(0,0,0,0.8,0.5,0.5);
     glutSwapBuffers();
 }
 
-
-void Move(unsigned char key, int x, int y)
-{
+void Mover(unsigned char key, int x, int y){ // Dos casos: Usando Vectores y librerias de OpenGL
    switch(key) {
 
+    /*vector<int> traslacion(vector<int> org,vector<int> traslado){
+        vector<int>res;
+        for (size_t i = 0; i < org.size(); i+=2)   {
+            res.push_back(org[i]+traslado[0]);
+            res.push_back(org[i+1]+traslado[1]);
+    }
+    return res;
+    }*/
 
     /// TRASLACION
     case 'w' :
@@ -54,6 +59,16 @@ void Move(unsigned char key, int x, int y)
         glTranslatef(0.1, 0 ,0.0);
         break;
 
+    /*vector<int> rotacion(vector<int> org,float theta){
+        vector<int>res;
+        float val = 3.14/180;
+        for (size_t i = 0; i < org.size(); i+=2)   {
+            res.push_back(org[i]*cos(theta*val) - org[i+1]*sin(theta*val));
+            res.push_back(org[i]*sin(theta*val) + org[i+1]*cos(theta*val));
+        }
+        return res;
+    }*/
+
     /// ROTACION
     case 'i' :
         cout<<"Arriba"<<endl;
@@ -72,11 +87,19 @@ void Move(unsigned char key, int x, int y)
         glRotatef(5.0, 0.0, 1.0, 0.0);
         break;
 
+    /*vector<int> escalamiento(vector<int> org,vector<float> escalar){
+        vector<int>res;
+        for (size_t i = 0; i < org.size(); i+=2)   {
+            res.push_back(org[i]*escalar[0]);
+            res.push_back(org[i+1]*escalar[1]);
+        }
+        return res;
+    }*/
+
     /// SCALAR
     case '+' :
         glScalef(2,2,2);
         break;
-
     case '-' :
         glScalef(0.5,0.5,0.5);
         break;
@@ -84,17 +107,14 @@ void Move(unsigned char key, int x, int y)
     glutPostRedisplay();
 }
 
-int main( int argc, char **argv )
-{
+int main( int argc, char **argv){
     glutInit( &argc, argv );
     glutInitDisplayMode( GLUT_RGBA | GLUT_DOUBLE );
     glutInitWindowSize( 350, 350 );
     glutInitWindowPosition(0, 0);
-    glutCreateWindow( "Linea 3D - Rotacion, Escalar y Traslacion" );
-
-    glutKeyboardFunc(Move);
-
-    glutDisplayFunc( display );
+    glutCreateWindow( "Rotacion, Escalar y Traslacion" );
+    glutKeyboardFunc(Mover);
+    glutDisplayFunc( Mostrar );
     glutMainLoop();
     return 0;
 }
